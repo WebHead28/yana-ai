df919a404f71216a2915409535d99bf988654c53
WebHead28 <khan.yusuf9596@gmail.com>
2026-05-05 05:38:49 +0530

    Delete Frontend directory

--- Frontend/yana-frontend/src/services/personalityService.js
+++
@@ -1,9 +0,0 @@
-import api from './api'
-
-export const personalityService = {
-  getQuestions: () =>
-    api.get('/personality/questions'),
-
-  submitAnswers: (answers) =>
-    api.post('/personality/submit', { answers }),
-}
